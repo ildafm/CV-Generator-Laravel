eb577169d0f41c58f1cb3ff969309b9bd23915f2
ildafm <ildafm4000@gmail.com>
2023-12-26 17:21:56 +0700

    Initial Commit

---
+++ cv_generator_laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->char('level', 1)->default('U'); // Kolom level dengan panjang 1 karakter dan default 'U'
+            $table->char('package', 1)->default('F'); // kolom paket dengan panjang 1 karakter dan default 'F'
+            $table->timestamp('package_active_at')->nullable();
+
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+
+            // $table->timestamps();
+
+            // Tambahkan default dan onUpdate untuk timestamps
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes(); // Tambahkan ini untuk soft delete
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
